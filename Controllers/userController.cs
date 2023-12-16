3c1afccf9084d00caa8fef54a1a40a64dda9b830
Kerimcan <464639@student.fontys.nl>
2023-12-16 19:23:34 +0100

    Add UserContext and update Get method in UserController

--- Controllers/userController.cs
+++ Controllers/userController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using User_Service.Models;
 
 namespace User_Service.Controllers
 {
@@ -6,10 +7,32 @@ namespace User_Service.Controllers
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Get()
         {
-            return Ok("Hello World");
+            var users = _context.Users.ToList();
+            return Ok(users);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(UserDTO userDTO)
+        {
+            return Ok(userDTO);
+        }
+
     }
 }
